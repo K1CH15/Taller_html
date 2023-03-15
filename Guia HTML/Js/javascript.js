dc90395b9b5c54e93e23d355d72c54291f4f79c1
KevinM2005 <ksmesa0@soy.sena.edu.co>
2023-03-15 14:47:16 -0500

    Guia completada

---
+++ Guia HTML/Js/javascript.js
@@ -0,0 +1,32 @@
+const time = document.getElementById('time');
+const date = document.getElementById('date');
+
+const monthNames = ["January", "February", "March", "April", "May", "June",
+    "July", "August", "September", "October", "November", "December"
+];
+
+const interval = setInterval(() => {
+
+    const local = new Date();
+    
+    let day = local.getDate(),
+        month = local.getMonth(),
+        year = local.getFullYear();
+
+    time.innerHTML = local.toLocaleTimeString();
+    date.innerHTML = `${day} ${monthNames[month]} ${year}`;
+
+}, 1000);
+
+var dialog = document.querySelector('dialog');
+dialogPolyfill.registerDialog(dialog);
+
+function showThisDialog() {
+  document.getElementById('this-dialog').show();
+}
+
+function hideThisDialog() {
+  document.getElementById('this-dialog').close();
+}
+
+
